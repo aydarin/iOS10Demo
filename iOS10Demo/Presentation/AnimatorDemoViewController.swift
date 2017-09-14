d74e43c44b295dcbea08190bc88e68b7f8d80aa3
Aydar Mukhametzyanov <aydar.mukh@gmail.com>
2017-09-14 22:43:20 +0200

    Add animated square

--- iOS10Demo/Presentation/AnimatorDemoViewController.swift
+++ iOS10Demo/Presentation/AnimatorDemoViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class AnimatorDemoViewController: UIViewController {
-
+    
+    private var animator: UIViewPropertyAnimator!
+    
+    @IBOutlet private weak var animatedView: UIView!
+    @IBOutlet private weak var verticalCenterConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var horisontalCenterConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        animator = UIViewPropertyAnimator(duration: 1, curve: .easeInOut, animations: nil)
+        
+        animator.addAnimations { [weak self] in
+            self?.verticalCenterConstraint.constant -= 100
+            self?.view.layoutIfNeeded()
+        }
+        
+        animator.addAnimations { [weak self] in
+            self?.horisontalCenterConstraint.constant += 100
+            self?.view.layoutIfNeeded()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animator.startAnimation()
     }
-
-
+    
 }
 
