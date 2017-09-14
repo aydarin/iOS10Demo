82bb2f7e1e7dac7be3c777511e2a6b1fc016f062
Aydar Mukhametzyanov <Aydar.Mukhametzyanov@Mac-Pro-M-031.local>
2017-09-14 09:09:31 +0200

    Initial commit

---
+++ iOS10Demo/Presentation/AnimatorDemoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AnimatorDemoViewController.swift
+//  iOS10Demo
+//
+//  Created by Aydar Mukhametzyanov on 9/14/17.
+//  Copyright © 2017 Aydar Mukhametzyanov. All rights reserved.
+//
+
+import UIKit
+
+class AnimatorDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
